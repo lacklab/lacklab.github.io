3178e4e59195e828545e4c1e8f92247b658245ab
pauldrinn <tahsinemirhan58771@gmail.com>
2025-01-22 05:04:07 +0000

    Deploying to gh-pages from @ lacklab/lacklab.github.io@0a7c41da36239ce55f0574286eaf7c218b95cbe0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -244,6 +244,216 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2023-01-ivancomp/";
+            },},{id: "news-our-snpstarrseq-paper-was-published-in-nar-so-we-went-curling",
+          title: 'Our snpSTARRseq paper was published in NAR so we went curling!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-02-snpstarrseq/";
+            },},{id: "news-dinner-and-a-surprise-for-nathan",
+          title: 'Dinner and a surprise for Nathan',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-02-nathanbd/";
+            },},{id: "news-congratulations-to-berkay-for-his-amazing-talk-at-the-vpc-terry-fox-seminar-series",
+          title: 'Congratulations to Berkay for his amazing talk at the VPC/Terry Fox Seminar Series...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-03-berkayterryfox/";
+            },},{id: "news-berkay-delivered-an-engaging-5-minute-lay-talk-at-the-big23-research-day",
+          title: 'Berkay delivered an engaging 5-minute lay talk at the BIG23 Research Day!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-03-berkaybig23/";
+            },},{id: "news-congratulations-to-emir-for-earning-the-2nd-best-presentation-award-in-the-kuttam-grc",
+          title: 'Congratulations to Emir for earning the 2nd Best Presentation Award in the KUTTAM...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-04-emirgrc/";
+            },},{id: "news-we-celebrated-doğancan-s-birthday-with-joy-and-laughter",
+          title: 'We celebrated Doğancan’s birthday with joy and laughter!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-04-dogancanbd/";
+            },},{id: "news-meric-kerim-and-ivan-presented-their-posters-at-the-17th-annual-lorne-d-sullivan-research-day",
+          title: 'Meric, Kerim, and Ivan presented their posters at the 17th Annual Lorne D....',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-06-lornedsullivan/";
+            },},{id: "news-we-enjoyed-a-lovely-dinner-with-nathan-in-richmond-at-once-more-restaurant",
+          title: 'We enjoyed a lovely dinner with Nathan in Richmond at Once More restaurant!...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-06-dinner/";
+            },},{id: "news-berkay-and-doğancan-showcased-their-artwork-at-the-artists-in-science-exhibition-in-the-vpc-atrium",
+          title: 'Berkay and Doğancan showcased their artwork at the “Artists in Science” exhibition in...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-06-artistsinscience/";
+            },},{id: "news-brandon-james-fasy-has-joined-the-turkish-dry-lab-team-as-a-m-sc-student",
+          title: 'Brandon James Fasy has joined the Turkish dry lab team as a M.Sc....',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-08-brandon/";
+            },},{id: "news-lack-lab-vancouver-had-a-fantastic-evening-playing-pool-and-joining-a-trivia-night-at-the-american-pub",
+          title: 'Lack Lab Vancouver had a fantastic evening playing pool and joining a trivia...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-09-pool/";
+            },},{id: "news-the-koç-team-observed-a-robotic-assisted-radical-prostatectomy-rarp",
+          title: 'The Koç team observed a Robotic-Assisted Radical Prostatectomy (RARP)!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-10-kuhrarp/";
+            },},{id: "news-happy-birthday-ivan",
+          title: 'Happy Birthday, Ivan!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-10-ivanbd/";
+            },},{id: "news-the-big-lack-lab-family-gathered-for-a-wonderful-meal-at-golden-swan-restaurant",
+          title: 'The big Lack Lab family gathered for a wonderful meal at Golden Swan...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-10-dinner/";
+            },},{id: "news-the-vancouver-team-triumphed-at-trivia-night-and-gave-a-warm-welcome-to-olka",
+          title: 'The Vancouver team triumphed at trivia night and gave a warm welcome to...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-11-trivianight/";
+            },},{id: "news-kerim-visited-the-lack-lab-in-istanbul",
+          title: 'Kerim visited the Lack Lab in Istanbul!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-12-kerimistanbul/";
+            },},{id: "news-lack-lab-celebrated-christmas-with-a-delightful-dinner-at-stepho-s-a-greek-restaurant",
+          title: 'Lack Lab celebrated Christmas with a delightful dinner at Stepho’s, a Greek restaurant!...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-12-christmas/";
+            },},{id: "news-we-enjoyed-a-festive-joint-secret-santa-celebration-with-the-lallous-lab",
+          title: 'We enjoyed a festive Joint Secret Santa celebration with the Lallous Lab!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-12-secretsanta/";
+            },},{id: "news-rosie-and-james-received-biotalent-funding-congratulations-we-celebrated-with-dinner-at-suika",
+          title: 'Rosie and James received BioTalent funding — congratulations! We celebrated with dinner at...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-02-biotalent/";
+            },},{id: "news-happy-birthday-olka-celebrated-with-delicious-tacos-at-la-taqueria-pinche-taco-shop",
+          title: 'Happy Birthday, Olka! Celebrated with delicious tacos at La Taqueria Pinche Taco Shop!...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-02-olkabd/";
+            },},{id: "news-lynn-and-brandon-gave-great-presentations-at-the-pharmacology-physiology-seminars-at-koc",
+          title: 'Lynn and Brandon gave great presentations at the Pharmacology-Physiology Seminars at Koc!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-02-kuttamseminars/";
+            },},{id: "news-congratulations-to-lynn-for-winning-the-best-poster-award-at-the-kuttam-grc-conference",
+          title: 'Congratulations to Lynn for winning the Best Poster award at the KUTTAM GRC...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-02-lynngrc/";
+            },},{id: "news-nathan-delivered-his-talk-at-the-spore-meeting",
+          title: 'Nathan delivered his talk at the SPORE meeting!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-03-nathanspore/";
+            },},{id: "news-dinner-at-meet-in-yaletown-happy-birthday-berkay-james-and-nathan",
+          title: 'Dinner at MeeT in Yaletown! Happy Birthday, Berkay, James, and Nathan!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-03-groupbd/";
+            },},{id: "news-kerim-delivered-an-inspiring-talk-at-the-terry-fox-seminar",
+          title: 'Kerim delivered an inspiring talk at the Terry Fox Seminar!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-03-kerimterryfox/";
+            },},{id: "news-emir-attended-the-igvf-mpra-workshop-and-presented-a-poster",
+          title: 'Emir attended the IGVF MPRA Workshop and presented a poster!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-03-emirigvf/";
+            },},{id: "news-wishing-ivan-the-best-as-he-heads-to-the-uk-as-a-visiting-researcher-at-adam-sharp-s-lab",
+          title: 'Wishing Ivan the best as he heads to the UK as a visiting...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-04-ivanicr/";
+            },},{id: "news-wishing-emir-the-best-as-he-heads-to-mcgill-as-a-visiting-researcher-with-yue-li",
+          title: 'Wishing Emir the best as he heads to McGill as a visiting researcher...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-05-emirmcgill/";
+            },},{id: "news-vancouver-witnessed-the-mesmerizing-northern-lights",
+          title: 'Vancouver witnessed the mesmerizing northern lights!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-05-northernlights/";
+            },},{id: "news-lack-lab-enjoyed-a-delightful-dinner-at-banana-leaf",
+          title: 'Lack Lab enjoyed a delightful dinner at Banana Leaf!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-06-dinner/";
+            },},{id: "news-berkay-meric-and-kerim-presented-at-the-18th-annual-lorne-d-sullivan-lectureship-and-research-day",
+          title: 'Berkay, Meric, and Kerim presented at the 18th Annual Lorne D. Sullivan Lectureship...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-06-lornedsullivan/";
+            },},{id: "news-berkay-presented-at-the-prostate-cancer-foundation-young-investigators-meeting",
+          title: 'Berkay presented at the Prostate Cancer Foundation Young Investigators Meeting!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-06-berkaypcf/";
+            },},{id: "news-meric-proudly-presented-at-the-terry-fox-seminars",
+          title: 'Meric proudly presented at the Terry Fox Seminars!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-06-mericterryfox/";
+            },},{id: "news-congratulations-to-rosie-and-james-on-completing-their-co-op-and-starting-their-internships",
+          title: 'Congratulations to Rosie and James on completing their Co-Op and starting their internships!...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-08-coopendjr/";
+            },},{id: "news-emir-visited-his-wet-lab-colleagues-in-vancouver",
+          title: 'Emir visited his wet lab colleagues in Vancouver!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-09-emirvan/";
+            },},{id: "news-happy-birthday-to-rosie-flora-emir-and-meric-we-had-a-wonderful-hotpot-dinner-together-in-happy-lamb",
+          title: 'Happy Birthday to Rosie, Flora, Emir, and Meric! We had a wonderful hotpot...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-09-groupbd/";
+            },},{id: "news-happy-birthday-flora-dinner-celebration-at-homura-yakiniku-japanese-bbq",
+          title: 'Happy Birthday, Flora! Dinner celebration at Homura Yakiniku, Japanese BBQ.',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-09-florabd/";
+            },},{id: "news-happy-birthday-ivan-celebrated-with-a-dinner-at-sun-bo-kong-a-vegetarian-chinese-restaurant",
+          title: 'Happy Birthday, Ivan! Celebrated with a dinner at Sun Bo Kong, a vegetarian...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-10-ivanbd/";
+            },},{id: "news-kerim-passed-his-comprehensive-exam-with-flying-colors",
+          title: 'Kerim passed his comprehensive exam with flying colors!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-10-kerimcomp/";
+            },},{id: "news-berkay-s-paper-has-been-published-in-nature-communications",
+          title: 'Berkay’s paper has been published in Nature Communications',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-11-berkaypaper/";
+            },},{id: "news-lack-lab-gathers-for-a-vancouver-dinner-at-tenen-an-eastern-european-restaurant",
+          title: 'Lack Lab gathers for a Vancouver dinner at TENEN, an Eastern European restaurant...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-12-dinner/";
+            },},{id: "news-lack-lab-warmly-welcomes-mayuko-and-daniel-with-a-dinner-at-akbar-joojeh-persian-restaurant",
+          title: 'Lack Lab warmly welcomes Mayuko and Daniel with a dinner at Akbar Joojeh...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2025-01-dinner/";
             },},{id: "team-bengul",
           title: 'Bengul',
           description: "",
@@ -259,11 +469,21 @@ ninja.data = [{
           description: "",
           section: "Team",handler: () => {
               window.location.href = "/team/betul/";
+            },},{id: "team-brandon",
+          title: 'Brandon',
+          description: "",
+          section: "Team",handler: () => {
+              window.location.href = "/team/brandon/";
             },},{id: "team-ceren",
           title: 'Ceren',
           description: "",
           section: "Team",handler: () => {
               window.location.href = "/team/ceren/";
+            },},{id: "team-daniel",
+          title: 'Daniel',
+          description: "",
+          section: "Team",handler: () => {
+              window.location.href = "/team/daniel/";
             },},{id: "team-derya",
           title: 'Derya',
           description: "",
@@ -314,6 +534,11 @@ ninja.data = [{
           description: "",
           section: "Team",handler: () => {
               window.location.href = "/team/ivan/";
+            },},{id: "team-james",
+          title: 'James',
+          description: "",
+          section: "Team",handler: () => {
+              window.location.href = "/team/james/";
             },},{id: "team-kerim",
           title: 'Kerim',
           description: "",
@@ -339,6 +564,16 @@ ninja.data = [{
           description: "",
           section: "Team",handler: () => {
               window.location.href = "/team/nathan/";
+            },},{id: "team-olka",
+          title: 'Olka',
+          description: "",
+          section: "Team",handler: () => {
+              window.location.href = "/team/olka/";
+            },},{id: "team-roselyn",
+          title: 'Roselyn',
+          description: "",
+          section: "Team",handler: () => {
+              window.location.href = "/team/roselyn/";
             },},{id: "team-shreyas",
           title: 'Shreyas',
           description: "",
@@ -385,30 +620,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://twitter.com/lack_lab", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
